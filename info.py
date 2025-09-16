f39f2552e81f3c0bf80a113313239820549ed7e8
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-16 19:21:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,11 +29,11 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 BOT_START_TIME = time()
 
 # Bot images & videos
-PICS = (environ.get('PICS', 'https://ibb.co/Z1xNkYQm')).split()
-REQ_PICS = (environ.get('REQ_PICS', 'https://ibb.co/C3PpG8gX')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://ibb.co/r2GBv87W")
-MELCOW_VID = environ.get("MELCOW_VID", "https://ibb.co/r2GBv87W")
-SPELL_IMG = environ.get("SPELL_IMG", "https://ibb.co/C3PpG8gX")
+PICS = (environ.get('PICS', 'https://envs.sh/t3L.jpg')).split()
+REQ_PICS = (environ.get('REQ_PICS', 'https://envs.sh/Zwc.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/t3L.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/t3L.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/Zwc.jpg")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2068233407 2098589219').split()]
